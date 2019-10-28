02b1ad05ddc561473d633192fdab919a73891510
xubuhui <1075051980@qq.com>
2019-10-28 18:59:41 +0800

    sql

---
+++ sql/admin.sql
@@ -0,0 +1,22 @@
+﻿# Host: 39.108.116.119  (Version: 5.5.60-MariaDB)
+# Date: 2019-10-28 18:58:36
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "admin"
+#
+
+CREATE TABLE `admin` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "admin"
+#
+
+INSERT INTO `admin` VALUES (1,'123456','e10adc3949ba59abbe56e057f20f883e');
